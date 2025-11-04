af90ef238403c59d9d2faacaecc9769d6380de7f
conceptspovdomestica-sys <conceptspovdomestica@gmail.com>
2025-11-04 16:23:26 -0400

    Add files via upload

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://procarni.com.ve/</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://procarni.com.ve/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://procarni.com.ve/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://procarni.com.ve/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://procarni.com.ve/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://procarni.com.ve/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
